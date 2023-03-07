acc80616e682b68b8d96b77387dcab2c3614492a
Onald Lumowa <onaldlumowa@hotmail.com>
2023-03-07 16:02:26 +0800

    Add Dropdown Navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+//   BUTTON SCROLL UP
+//   ===================
+  // Get the button
+  let mybutton = document.getElementById("myBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
 // fungtion navbar highlight scroll mouse
 $(document).ready(function() {
     $(window).scroll(function(){
@@ -133,3 +155,4 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+
